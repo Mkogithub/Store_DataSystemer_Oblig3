69022f16be230627689a845f04c74935b6df85ab
Mkogithub <Mkogithub@users.noreply.github.com>
2022-11-09 15:29:38 +0100

    initial commit v2 heheh

---
+++ rest_api/src/test/kotlin/com/example/Oblig_3/rest_api/RestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Oblig_3.rest_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
